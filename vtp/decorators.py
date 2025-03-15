2438291b01f66d19961593bc2c41edf8258f3b61
Lawalfortunate3 <shoaibshoukatbhatti99@gmail.com>
2025-03-15 19:37:21 +0100

    go

--- vtp/decorators.py
+++ vtp/decorators.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 def unauthenticated_user(view_func):
  def wrapper_func(request, *args, **kwargs):
     if request.user.is_authenticated:
-        return redirect('VTU')
+        return redirect('vtp')
     else:
         return view_func (request, *args, **kwargs)
 
